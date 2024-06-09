e85b0e8ed6ebdfaa3ab7ca252f2f85a313de081a
Iziel <irinqum@ncsu.edu>
2024-06-09 14:22:54 -0400

    1

---
+++ Calculator.java
@@ -0,0 +1,44 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Calculator implements ActionListener{
+
+	JFrame frame;
+	JTextField textfield;
+	JButton[] numberButtons = new JButton[10];
+	JButton[] functionButtons = new JButton[9];
+	JButton addButton,subButton,mulButton,divButton;
+	JButton decButton, equButton, delButton, clrButton, negButton;
+	JPanel panel;
+	
+	Font myFont = new Font("Segoe", Font.PLAIN, 30);
+	
+	double num1=0,num2=0,result=0;
+	char operator;
+
+    Calculator() {
+        frame = new JFrame("Calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420, 550);
+        frame.setLayout(null);
+
+        textfield = new JTextField();
+        textfield.setBounds(50,25,300,50);
+        textfield.setFont(myFont);
+        textfield.setEditable(true);
+        
+        frame.add(textfield);
+        frame.setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    
+    }
+}
